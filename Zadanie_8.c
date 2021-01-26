e0c8e93ebac87c4692931dca0ae1748c5d6108c6
Krystiankr <56454269+Krystiankr@users.noreply.github.com>
2021-01-26 22:17:17 +0100

    Create Zadanie_8.c

---
+++ Zadanie_8.c
@@ -0,0 +1,29 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <ctime>
+
+using namespace std;
+
+int main() {
+	printf("Zadanie 7\n");
+
+	srand(time(NULL));
+	int tab[100][50];
+
+	for (int i = 0; i < 100; i++) {
+		for (int j = 0; j < 50; j++) {
+			tab[i][j] = rand() % 100;
+		}
+	}
+	int wiersz, kolumna;
+	cout << "Podaj wiersz: ";
+	cin >> wiersz;
+	cout << "\nPodaj kolumna: ";
+	cin >> kolumna;
+	if (wiersz > 0 and wiersz < 100 and kolumna > 0 and kolumna < 50) {
+		printf("Wartosc z wiersza %d i kolumny %d wynosi: %d", wiersz, kolumna, tab[wiersz][kolumna]);
+
+	}
+	else printf("Wprowadziles dane z poza tablicy.");
+
+}
